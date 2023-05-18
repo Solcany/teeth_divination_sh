c17dfac6cb6843ea402f4b54f92fe709ffdf0a77
m <matus.solcany@protonmail.com>
2023-05-18 14:36:47 +0200

    fonts

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 	sudo ~/rpi-rgb-led-matrix/utils/text-scroller \
-	-f ~/teeth_divination_sh/i.bdf \
-	-s 5 \
+	-f ~/teeth_divination_sh/baskerville.bdf \
+	-s 2 \
+	-t -1 \	
 	--led-rows=64 \
 	--led-cols=64 \
 	--led-chain=4 \
 	--led-gpio-mapping=adafruit-hat \
 	--led-slowdown-gpio=5 \
-	"IGNORE ALIEN ORDERS"
+	"Though your love may be faltering, there is admiration and respect in the eyes of those who see you truly. Your disapproval may be a heavy weight, but it is a sign of the times"
