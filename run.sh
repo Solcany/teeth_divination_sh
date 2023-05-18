59b554e52bf4413361ff0476bbaf3df78a71d0f2
m <matus.solcany@protonmail.com>
2023-05-18 13:39:25 +0200

    run

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-cd ~/led_matrix/examples_api_use
+	sudo ~/rpi-rgb-led-matrix/utils/text-scroller \
+	-f ~/fonts/i.bdf \
+	-s 5 \
+	--led-rows=64 \
+	--led-cols=64 \
+	--led-chain=4 \
+	--led-gpio-mapping=adafruit-hat \
+	--led-slowdown-gpio=4 \
+	--led-limit-refresh=100 \
+	"IGNORE ALIEN ORDERS"
